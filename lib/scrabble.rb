45372691c59a25beade36c15ff4c6b3343b4af4c
Tim Tyrrell <timomitchel23@gmail.com>
2017-08-30 10:58:20 -0600

    Scrabble not finished
    `		!!!
    quit
    exit

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,18 +1,51 @@
 class Scrabble
 
-  def score(word)
-    1
+  def initialize
+    @array_of_word = []
+    @count = 0
   end
 
-  def point_values
-    {
+  def score(word)
+    if word.length > 6
+      @count += 10
+    end
+    @array_of_word =[]
+    word.split('').each do |letter|
+      @array_of_word << letter.upcase
+    end
+      point_counter(@array_of_word)
+
+  end
+
+  def point_counter(array)
+    points =  array
+    points.map {|letters| SCRABBLE_POINTS.values_at(letters)}
+
+    summed_array = points.inject(0){|sum,x| sum + x }
+    require 'pry'; binding.pry
+    @count += summed_array
+
+  end
+
+
+
+
+SCRABBLE_POINTS =     {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
       "M"=>3, "N"=>1, "O"=>1, "P"=>3,
       "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+      "Y"=>4, "Z"=>10, "" => 0, nil => 0
     }
-  end
+
+
+
+
 end
+
+x = Scrabble.new
+points = x.score("APPLE")
+points
+puts @count
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -10,4 +10,21 @@ class ScrabbleTest < Minitest::Test
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_it_can_play_empty_string_for_0_points
+    assert_equal 0, Scrabble.new.score('')
+  end
+
+  def test_it_can_pass_or_play_nil
+  end
+
+  def test_it_can_score_a_word_with_multipliers
+  end
+
+  def test_7_letter_word_adds_10_points
+  end
+
+
+
+
 end
